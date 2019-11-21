7f3b6380d12d44533952ee94e064ea84ab3c6b0e
InJoong <injoong01@hotmail.com>
2019-11-20 23:10:52 -0600

    Se agrego un archivo de nuevo de soko donde se puede ver un juego con personajes en movimiento a reaccion de inputs de usuarios

--- canvas.c
+++ canvas.c
@@ -1,13 +1,29 @@
-
+#include <stdio.h>
+#include <poll.h>
+#include <unistd.h>
 #include <ncurses.h>
 
-int main()
-{	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+int main (void)
 
-	return 0;
+{
+		struct pollfd mypoll = { STDIN_FILENO, POLLIN|POLLPRI };
+	
+        /* compile with gcc -lncurses file.c */
+        int c = 0;
+        /* Init ncurses mode */
+        initscr ();
+        /* Hide cursor */
+        curs_set (0);
+        while (c < 1000) {
+                /* Print at row 0, col 0 */
+                mvprintw (0, 0, "%d", c++);
+                refresh ();
+                if( poll(&mypoll, 1, 2000) )
+				{
+					getch();
+				}
+        }
+        /* End ncurses mode */
+        endwin();
+        return 0;
 }
